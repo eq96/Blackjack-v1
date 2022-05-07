50e032851bbdf3e30aaf1e2e865ac7a2f27a96a8
eq96 <edquiroga96@gmail.com>
2022-05-06 20:53:07 -0500

    Update script.js
    Sharpened logic

--- script.js
+++ script.js
@@ -8,12 +8,26 @@ let firstCard = randomNum(10);
 let secondCard = randomNum(10);
 let sum = firstCard + secondCard;
 
+let hasBlackJack = false;
+let isAlive = true;
+let message = ""
+
 if (sum > 21) {
-    console.log(`You hit ${sum}. You bust!`)
+    message = `You hit ${sum}. You bust!`
+    isAlive = false;
 }
 else if (sum === 21) {
-    console.log(`You hit ${sum}. Blackjack! You Win!`)
+    message = `You hit ${sum}. Blackjack! You Win!`
+    hasBlackJack = true;
 }
-else if (sum < 21) {
-    console.log(`You hit ${sum}. Hit or settle?`)
+else {
+    message = `You hit ${sum}. Hit or settle?`
 }
+
+if (hasBlackJack === true) {
+    message = `Here are your winnings. Would you like to place another bet?`
+} else if (isAlive === false) {
+    message = `Press ENTER to play again!`
+}
+
+console.log(message)
